58c443f3ed21297651b4ef9420b2a2dc6ebcb9a2
eunae <dmsdo1234@nate.com>
2017-11-16 23:58:57 +0900

    second commit

--- Documents/workspace/codelab-tut/src/components/ContactDetails.js
+++ Documents/workspace/codelab-tut/src/components/ContactDetails.js
@@ -3,36 +3,26 @@ import React from 'react';
 export default class ContactDetails extends React.Component {
     render() {
 
-        // 선택되었을 때 보여질 부분
         const details = (
             <div>
-                <p>{ this.props.contact.name }</p>
-                <p>{ this.props.contact.phone }</p>
+                <p>{this.props.contact.name}</p>
+                <p>{this.props.contact.phone}</p>
             </div>
         );
+        const blank = (<div>Not Selected</div>);
 
-        // 아무것도 선택되지 않았을 때 보여질 부분
-        const blank = (
-            <div> Nothing is Selected </div>
-        );
-        console.log(this.props.contact)
-        return(
+        return (
             <div>
                 <h2>Details</h2>
-
-                { /* isSelected props 값에 따라 어떤걸 보여줄지 정한다
-                    ternary expression condition ? true : false */ }
-
-                { this.props.isSelected ? details : blank }
-
-             </div>
+                {this.props.isSelected ? details : blank}
+            </div>
         );
     }
 }
 
 ContactDetails.defaultProps = {
     contact: {
-        name: "",
-        phone: ""
+        name: '',
+        phone: ''
     }
-}
+};
